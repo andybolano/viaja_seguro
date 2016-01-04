2a209f8429d16cf020804ede5a55184f8e612536
Andy Yair Bolaño Castilla <andy_yair@live.com>
2016-01-04 07:02:56 -0500

    primer commit

---
+++ public/scripts/controllers/indexController.js
@@ -0,0 +1,13 @@
+app.controller('indexController', function($scope) {
+    hoy();
+    function hoy() {
+        var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+        var diasSemana = new Array("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado");
+        var f = new Date();
+        var hoy = diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear();
+        $scope.hoy = hoy;
+    }
+
+});
+
+
---
+++ public/scripts/route_superadmin.js
@@ -0,0 +1,61 @@
+var uri = "../../public";
+var app;
+(function(){
+    app = angular.module("superadmin", ['ngRoute','ui.keypress']);
+    
+    app.config(['$routeProvider', '$locationProvider', function AppConfig($routeProvider, $locationProvider){
+            
+            
+            $routeProvider
+          
+                  
+                    .when("/home", {
+                        templateUrl: 'home.html'
+                    })
+                    .otherwise({
+                        redirectTo:"/default"
+                    });
+                    
+            
+    }]);
+
+    app.directive('ngEnter', function () {
+        return function (scope, elements, attrs) {
+            elements.bind('keydown keypress', function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+    
+    app.filter('ifEmpty', function() {
+        return function(input, defaultValue) {
+            if (angular.isUndefined(input) || input === null || input === '') {
+                return defaultValue;
+            }
+
+            return input;
+        };
+    });
+    
+    app.directive('uploaderModel',['$parse',function($parse){
+        return{
+            restrict: 'A',
+            link: function(scope,iElement,iAttrs){
+                iElement.on('change',function(e)
+                {
+                    $parse(iAttrs.uploaderModel).assign(scope,iElement[0].files[0]);
+                });
+            }
+        };
+
+    }]);
+
+
+})();
+
+
