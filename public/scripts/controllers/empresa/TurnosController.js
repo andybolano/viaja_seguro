df39daa8b65c6432989c5b19f4ac5545c4b2cc81
Gustavo Pérez Machado <gustperz@gmail.com>
2016-01-30 01:40:51 -0500

    diseño vista de turnos de conductores, aun no funcional

--- public/scripts/controllers/empresa/TurnosController.js
+++ public/scripts/controllers/empresa/TurnosController.js
@@ -6,18 +6,36 @@ app.controller('TurnosController', TurnosController);
 function TurnosController($scope, turnosService){
 
     $scope.conductores = [];
+    $scope.selectedConductor = {};
+    $scope.rutas = [];
+    $scope.selectedRuta = {};
 
-    $scope.models = {
-        selected: null,
-        lists: {"conductores": [], "B": []}
-    };
+    $scope.addConductor = addConductor;
+    $scope.selectConductor = selectConductor;
 
-    // Model to JSON for demo purpose
-    $scope.$watch('models', function(model) {
-        $scope.modelAsJson = angular.toJson(model, true);
-    }, true);
+    function addConductor(ruta){
+        $scope.selectedRuta = ruta;
+        $scope.selectedRuta.conductores = [];
+        cargarConductores();
+        $("#modalBuscarconductor").openModal();
+    }
 
-    cargarConductores();
+    function selectConductor (conductor){
+        $scope.selectedRuta.conductores.push(conductor);
+        $("#modalBuscarconductor").closeModal();
+    }
+
+    cargarRutas();
+    function cargarRutas() {
+        turnosService.getRutasCentral().then(success, error);
+        function success(p) {
+            $scope.rutas = p.data;
+        }
+        function error(error) {
+            console.log('Error al cargar conductores', error);
+        }
+    }
+
     function cargarConductores() {
         turnosService.getAllConductores().then(success, error);
         function success(p) {
