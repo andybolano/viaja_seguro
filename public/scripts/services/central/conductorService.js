5424deb0639aa4faa5ff5dd8b29e18052165a45b
Gustavo Pérez Machado <gustperz@gmail.com>
2016-02-29 16:14:06 -0500

    eliminacion de archivos no usados

--- public/scripts/services/central/conductorService.js
+++
@@ -1,7 +0,0 @@
-app.service('conductorService', function ($http , authService) {
-    var myuri = uri + '/api/centrales/'+authService.currentUser().central.id+'/conductores';
-
-    this.getAll = function  () {
-        return $http.get(myuri);
-    }
-});
--- public/scripts/services/central/ubicacionesService.js
+++
@@ -1,7 +0,0 @@
-app.service('ubicacionesService', function ($http ) {
-
-    this.getUbicacionConductores = function(ruta_id){
-        return $http.get(uri + '/api/conductores/rutas/'+ruta_id+'/ubicacion');
-    }
-
-});
