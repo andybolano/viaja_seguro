e848074642c4eb5205be301c4d8ec04708ac02e7
Gustavo Pérez Machado <gustperz@gmail.com>
2016-05-26 13:31:10 -0500

    server socket

--- servsocketubicaciones/socket.js
+++ servsocketubicaciones/socket.js
@@ -16,24 +16,28 @@ module.exports = function (io) {
         });
 
         socket.on("loginCliente", function (data) {
-            console.log(socket.id+' cliente escuchando '+ data.cliente_id);
-            clientes[data.conductor_id] || (clientes[data.conductor_id] = []);
-            clientes[data.conductor_id].push({id: socket.id});
+            console.log(socket.id+' cliente escuchando '+ data.conductor_id);
+            clientes_conductores[data.conductor_id] || (clientes_conductores[data.conductor_id] = []);
+            clientes_conductores[data.conductor_id].push({id: socket.id});
+            console.log('total clientes conductor '+ data.conductor_id+' :'+ clientes_conductores[data.conductor_id].length);
         });
 
         socket.on("posConductor", function (data) {
             var monitor = monitores[data.ruta_id];
-            console.log(socket.id+' send pos to:');
-            console.log(monitor);
-            console.log('\ndata :\n', data);
             if (monitor) {
+                console.log('conductor '+data.conductor_id+' send pos to ruta: ' + monitor);
+                // console.log(monitor);
+                console.log('\ndata :\n', data);
                 io.sockets.to(monitor.id).emit('updatePos', data);
-                var clientes = clientes_conductores[data.conductor_id];
-                if (clientes) {
-                    for (var i = 0; i < clientes.length; i++) {
-                        io.sockets.to(clientes[i].id).emit('updatePos', data);
-                    }
+            }
+            var clientes = clientes_conductores[data.conductor_id];
+            if (clientes.length) {
+                console.log('clientes: ');
+                for (var i = 0; i < clientes.length; i++) {
+                    console.log(clientes[i].id+' ');
+                    io.sockets.to(clientes[i].id).emit('updatePos', data);
                 }
+                console.log('\n');
             }
         });
 
