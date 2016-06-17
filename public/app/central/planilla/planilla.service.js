abc05dce38d8cb3567eda398b9bd70f8e4a191a5
Jose M. Soto <jomisoac@gmail.com>
2016-06-16 19:17:08 -0500

    Fix servide planilla

--- public/app/central/planilla/planilla.service.js
+++ public/app/central/planilla/planilla.service.js
@@ -6,6 +6,10 @@
         .service('planillasService', planillasService);
 
     function planillasService($http, authService, API) {
+        this.getDeducciones = function () {
+            return $http.get(API + '/empresas/'+authService.currentUser().central.empresa.id+'/deducciones');
+        }
+
         this.getPlanillas = function () {
             var req = $http.get(API + '/centrales/' + authService.currentUser().central.id + '/planillas');
             return req;
