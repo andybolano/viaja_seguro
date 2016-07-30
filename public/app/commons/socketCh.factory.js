85792fbd4d5875773c1c92127d6044e478ec743a
Jose M. Soto <jomisoac@gmail.com>
2016-07-30 04:11:10 -0500

    cambios

--- public/app/commons/socketCh.factory.js
+++ public/app/commons/socketCh.factory.js
@@ -22,7 +22,7 @@
         return service;
 
         function connect() {
-            socket = io.connect('http://dev.viajaseguro.co:8070')
+            socket = io.connect('http://dev.viajaseguro.co:8007')
             sessionId = socket.io.engine.id;
         }
 
