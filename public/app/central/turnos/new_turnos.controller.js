2f348350917f0b91f31981d344180617495772cd
Jose MiguelSoto Acosta <jomisoac@gmail.com>
2016-08-17 15:29:41 -0500

    cambios

--- public/app/central/turnos/new_turnos.controller.js
+++ public/app/central/turnos/new_turnos.controller.js
@@ -649,8 +649,8 @@
 
         //GIROS
         function refrescarGiros(conductor_id) {
-            document.getElementById("guardarG").disabled = false;
-            document.getElementById("actualizarG").disabled = true;
+            // document.getElementById("guardarG").disabled = false;
+            // document.getElementById("actualizarG").disabled = true;
             newTurnosService.refrescarGiros(conductor_id).then(success, error);
             function success(p) {
                 vm.listaGiros = [];
@@ -691,8 +691,8 @@
         }
 
         function cargarModificarGiro(item) {
-            document.getElementById("actualizarG").disabled = false;
-            document.getElementById("guardarG").disabled = true;
+            // document.getElementById("actualizarG").disabled = false;
+            // document.getElementById("guardarG").disabled = true;
             vm.Giros = item;
         };
 
@@ -701,8 +701,8 @@
             function success(p) {
                 Materialize.toast(p.data.message, '5000', "rounded");
                 refrescarGiros(vm.conductor.id);
-                document.getElementById("guardarG").disabled = false;
-                document.getElementById("actualizarG").disabled = true;
+                // document.getElementById("guardarG").disabled = false;
+                // document.getElementById("actualizarG").disabled = true;
             }
 
             function error(error) {
@@ -832,8 +832,8 @@
 
         //PAQUETES
         function refrescarPaquetes(conductor_id) {
-            document.getElementById("guardarP").disabled = false;
-            document.getElementById("actualizarP").disabled = true;
+            // document.getElementById("guardarP").disabled = false;
+            // document.getElementById("actualizarP").disabled = true;
             newTurnosService.refrescarPaquetes(conductor_id).then(success, error);
             function success(p) {
                 vm.listaPaquetes = [];
@@ -875,8 +875,8 @@
         }
 
         function cargarModificarPaquete(item) {
-            document.getElementById("actualizarP").disabled = false;
-            document.getElementById("guardarP").disabled = true;
+            // document.getElementById("actualizarP").disabled = false;
+            // document.getElementById("guardarP").disabled = true;
             vm.Paquetes = item;
         };
 
@@ -885,8 +885,8 @@
             function success(p) {
                 Materialize.toast(p.data.message, '5000', 'rounded');
                 refrescarPaquetes(vm.conductor.id);
-                document.getElementById("guardarP").disabled = false;
-                document.getElementById("actualizarP").disabled = true;
+                // document.getElementById("guardarP").disabled = false;
+                // document.getElementById("actualizarP").disabled = true;
             }
 
             function error(error) {
@@ -1185,14 +1185,14 @@
 
         function limpiarGiros(conductor_id) {
             document.getElementById("guardarG").disabled = false;
-            document.getElementById("actualizarG").disabled = true;
+            // document.getElementById("actualizarG").disabled = true;
             refrescarGiros(conductor_id)
             vm.Giros = "";
         };
 
         function limpiarPaquetes(conductor_id) {
             document.getElementById("guardarP").disabled = false;
-            document.getElementById("actualizarP").disabled = true;
+            // document.getElementById("actualizarP").disabled = true;
             refrescarPaquetes(conductor_id)
             vm.Paquetes = "";
         };
